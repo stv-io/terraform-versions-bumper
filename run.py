09c81f53520290db2e1748a419318279d8cea5ee
Stephen Attard <stephen.attard@opsguru.io>
2023-04-27 08:24:46 +0200

    Various improvements

--- run.py
+++ run.py
@@ -1,58 +1,72 @@
 import glob
 import fileinput
 import os
+import shutil
 import sys
 import logging
 import pygohcl
 import httpx
 import json
+import argparse
 
-# log_format = "%(asctime)s %(levelname)s: %(message)s"
-log_format = "%(levelname)s: %(message)s"
-logging.basicConfig(format=log_format, level=logging.INFO)
-log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
-logging.getLogger().setLevel(log_level)
-httpx_logger = logging.getLogger("httpx")
-httpx_logger.setLevel(log_level)
-urllib3_logger = logging.getLogger("urllib3")
-urllib3_logger.setLevel(log_level)
-
-# TODO: Configure this from an input parameter
-replace = True
-
-# TODO: cater for when we don't have matching terraform files, or we don't find the terraform block
-
+# TODO: cater for when we don't have matching terraform files,
+# or we don't find the terraform block
 
 def main():
+    parser = argparse.ArgumentParser(description="Bump terraform required versions and provider versions to latest releases")
+    parser.add_argument("--dry-run", "-r", dest='dry_run', help="Dry run, don't modify files",action="store_true")
+    parser.add_argument("--backup" , "-b", dest='backup', help="create backup files with a suffix", action="store_true")
+    parser.add_argument("--display", "-d", dest='display', help="display matched files and contents", action="store_true")
+    parser.add_argument("--verbose", "-v", dest='verbose', help="display matched files and contents", action="store_true")
+    args = parser.parse_args()
+
+    dry_run = args.dry_run
+    backup = args.backup
+    display = args.display
+    verbose = args.verbose
+
+    print(f"dry_run: {dry_run}, backup: {backup}, display: {display}, verbose: {verbose}")
+
+    log_format = "%(levelname)s: %(message)s"
+    logging.basicConfig(format=log_format, level=logging.INFO)
+    log_level = "DEBUG" if verbose else "INFO"
+    logging.getLogger().setLevel(log_level)
+    httpx_logger = logging.getLogger("httpx")
+    httpx_logger.setLevel(log_level)
+    urllib3_logger = logging.getLogger("urllib3")
+    urllib3_logger.setLevel(log_level)
+
     # Terraform versions
-    logging.info(f'Searching for "required_version" in terraform files ..')
+    logging.info("Searching for \"required_version\" in all *.tf files ..")
     tf_required_filename = find_file_with_string("required_version", "*.tf")
+    if backup:
+        backup(tf_required_filename)
     logging.info(f"Processing {tf_required_filename}..")
     tf_required_block = load_terraform_block(tf_required_filename)
     current_tf_version_string = resolve_tf_version(tf_required_block)
-    # print(f"current_tf_version_string {current_tf_version_string}")
     current_tf_version = resolve_version(current_tf_version_string)
     logging.info(f"current_tf_version {current_tf_version}")
     latest_terraform_release = get_latest_terraform_release()
     latest_terraform_version = latest_terraform_release.replace("v", "")
-    # print(f"latest_terraform_version {latest_terraform_version}")
-    # print(f"tf_required_filename {tf_required_filename}")
-    # print(f"current_tf_version {current_tf_version}")
-    # print(f"latest_terraform_version {latest_terraform_version}")
 
-    if replace:
+    if not dry_run:
         logging.info(f"Replacing terraform required_version in {tf_required_filename}")
         bump_version_latest(
             tf_required_filename, current_tf_version, latest_terraform_version
         )
+    if display:
+        cat(tf_required_filename)
 
-    # # Providers versions
-    logging.info(f'Searching for "required_providers" in terraform files ..')
+    # Providers versions
+    logging.info("Searching for \"required_providers\" in all *.tf files ..")
     providers_required_filename = find_file_with_string("required_providers", "*.tf")
+    if backup:
+        backup(providers_required_filename)
+
     providers_required_block = load_terraform_block(providers_required_filename)
     provider_map = resolve_providers(providers_required_block)
     logging.debug(f"found {len(provider_map)} providers\n{json.dumps(provider_map)}")
-    for provider_name, provider_config in provider_map.items():
+    for provider_config in provider_map.values():
         source = provider_config["source"]
         version = provider_config["version"]
         logging.info(f"Processing provider {source}")
@@ -61,36 +75,66 @@ def main():
             f"Current defined versions - {version}; latest available version for provider {source}: {latest_provider_version}"
         )
         provider_version = resolve_version(version)
-        if replace:
+        if not dry_run:
             logging.info(
                 f"Replacing required_providers in {providers_required_filename}"
             )
             bump_version_latest(
                 providers_required_filename, provider_version, latest_provider_version
             )
+        if display:
+            cat(providers_required_filename)
+
+
+def backup(filename):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    src_path = os.path.join(current_dir, filename)
+    dst_path = os.path.join(current_dir, filename + ".bak")
+    shutil.copy(src_path, dst_path)
+
+
+def cat(filename):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    abs_file = os.path.join(current_dir, filename)
+    logging.info(f"Displaying the contents of {abs_file}")
+    with open(abs_file, 'r') as file:
+        file_contents = file.read()
+    sys.stdout.write(file_contents)
+    print("\n")
+
+
+def delete(filename):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    abs_file = os.path.join(current_dir, filename)
+    logging.info(f"Deleting {abs_file}")
+    os.remove(abs_file)
 
 
 def bump_version_latest(file_name, search_string, replace_string):
     logging.info(
         f"Replacing version {search_string} with {replace_string} in {file_name}"
     )
-    with fileinput.FileInput(file_name, inplace=True, backup=".bak") as file:
+    with fileinput.FileInput(file_name, inplace=True, backup=False) as file:
         for line in file:
             print(line.replace(search_string, replace_string), end="")
 
 
 def find_file_with_string(string, extension):
     files = glob.glob(extension)
-    for line in fileinput.input(files):
-        if string in line:
-            logging.debug(f"Matched {string} in {files}")
-    if len(files) != 1:
+    files_matched = []
+    for file in files:
+        with open(file, 'r') as f:
+            if string in f.read():
+                logging.info(f'matched string {string} in file {file}')
+                files_matched.append(file)
+
+    if len(files_matched) != 1:
         logging.error(
-            f'The string "{string}" was found in {len(files)} files: {files}.'
+            f'The string "{string}" was found in {len(files_matched)} files: {files_matched}.'
         )
         logging.error('Please run "terraform validate", fix, and try again')
         sys.exit(1)
-    return files[0]
+    return files_matched[0]
 
 
 def load_terraform_block(file):
@@ -109,63 +153,53 @@ def resolve_tf_version(content):
 
 
 def resolve_providers(content):
-    provider_map = {}
-    if "terraform" in content:
-        terraform = content["terraform"]
-        if "required_providers" in terraform:
-            required_providers = terraform["required_providers"]
-            for provider_name, provider_config in required_providers.items():
-                if "source" in provider_config and "version" in provider_config:
-                    provider_map[provider_name] = {
-                        "source": provider_config["source"],
-                        "version": provider_config["version"],
-                    }
-    return provider_map
+    terraform = content.get("terraform", {})
+    required_providers = terraform.get("required_providers", {})
+    return {
+        name: {
+            "source": config["source"],
+            "version": config["version"]
+        }
+        for name, config in required_providers.items()
+        if "source" in config and "version" in config
+    }
 
 
 def get_latest_terraform_release():
     url = "https://api.github.com/repos/hashicorp/terraform/releases/latest"
     with httpx.Client() as client:
-        response = httpx.get(url)
+        response = client.get(url)
         logging.debug(f"Response status code: {response.status_code}")
-    if response.status_code == 200:
-        data = response.json()["tag_name"]
-    else:
-        print(f"{response.status_code} error querying {url}:\n {response.text}")
-    return data
+    try:
+        response.raise_for_status()
+        return response.json()["tag_name"]
+    except httpx.HTTPError as e:
+        logging.error(f"{response.status_code} {e} error querying {url}:\n {response.text}")
+        return None
 
 
 def get_latest_provider(source):
     url = f"https://registry.terraform.io/v2/providers/{source}/provider-versions"
     with httpx.Client() as client:
-        response = httpx.get(url)
+        response = client.get(url)
         logging.debug(f"Response status code: {response.status_code}")
-
-    if response.status_code == 200:
-        data = response.json()["data"]
-        for version in data:
-            latest_version = data[-1]["attributes"]["version"]
-    else:
-        print(f"{response.status_code} error querying {url}:\n {response.text}")
-
-    return latest_version
-
+    try:
+        response.raise_for_status()
+        return response.json()["data"][-1]["attributes"]["version"]
+    except httpx.HTTPError as e:
+        logging.error(f"{response.status_code} {e} error querying {url}:\n {response.text}")
+        return None
 
 def resolve_version(version_string):
-    # print(f"version_string {version_string}")
     version_parts = version_string.split(" ")
-    # print(f"version_parts {version_parts}")
     if len(version_parts) == 2:
         version = version_parts[1]
-        print(f"version {version}")
     elif len(version_parts) == 1:
-        print("lenght is 1")
         if version_parts[0].isnumeric() or isfloat(version_parts[0]):
             version = version_parts[0]
     else:
         logging.error(f"Version {version_string} not supported")
         sys.exit(1)
-    # logging.debug(version)
     return version
 
 

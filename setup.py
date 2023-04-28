dadbee3a885510b09dc0238b448e94e9826a347e
Stephen Attard <stephen.attard@opsguru.io>
2023-04-28 17:41:16 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='terraform-versions-bumper',
-    version='0.0.2',
+    version='0.0.3',
     description='CLI tool to update terraform and provider versions in tf files',
     scripts=['src/terraform-versions-bumper'],
     install_requires=['httpx==0.24.0', 'pygohcl==1.0.6'],

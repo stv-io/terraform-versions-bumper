5dbac94509d47f57f82588774442f2e4a5c0fcfa
Stephen Attard <stephen.attard@opsguru.io>
2023-04-28 09:17:25 +0200

    Test with old versions

--- tests/providers.tf
+++ tests/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.63.1"
+      version = "~> 4.53.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.63.1"
+      version = "~> 4.53.1"
     }
   }
 }

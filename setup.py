5fa3e3871f487c9c44280b7ebbde5fb74dfc2b9c
Stephen Attard <stephenattard86@gmail.com>
2025-06-24 08:37:14 +0200

    chore: freshen up versions

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('requirements.txt') as f:
 
 setup(
     name='terraform-versions-bumper',
-    version='0.0.3',
+    version='0.0.4',
     description='CLI tool to update terraform and provider versions in tf files',
     scripts=['src/terraform-versions-bumper'],
-    install_requires=['httpx==0.24.0', 'pygohcl==1.0.6'],
+    install_requires=['httpx==0.28.1', 'pygohcl==1.2.5'],
     python_requires='>=3.9'
 )
